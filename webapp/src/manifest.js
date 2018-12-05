ddb6c1808cfb66d4a5bffd84b3c3f67dc4601046
Hanzei <16541325+hanzei@users.noreply.github.com>
2018-12-05 19:27:53 +0100

    Change plugin id to zoom (#12)

--- webapp/src/manifest.js
+++ webapp/src/manifest.js
@@ -1,5 +1,2 @@
-// Copyright (c) 2017-present Mattermost, Inc. All Rights Reserved.
-// See License for license information.
-
-export const id = 'com.mattermost.zoom';
+export const id = 'zoom';
 export const version = '1.0.2';
