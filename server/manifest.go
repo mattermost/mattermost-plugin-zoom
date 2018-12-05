1b61a745502d2b92a717b793b60566c0e92dce53
Hanzei <hanzei@mailbox.org>
2018-12-05 14:59:22 +0100

    Change plugin id to zoom

--- server/manifest.go
+++ server/manifest.go
@@ -1,12 +1,9 @@
-// Copyright (c) 2017-present Mattermost, Inc. All Rights Reserved.
-// See License for license information.
-
 package main
 
 var manifest = struct {
 	Id      string
 	Version string
 }{
-	Id:      "com.mattermost.zoom",
+	Id:      "zoom",
 	Version: "1.0.2",
 }
