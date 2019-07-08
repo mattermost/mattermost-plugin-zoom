90d2a8b45b7eccb1a26decef94895df27d8320a2
Christopher Speller <crspeller@gmail.com>
2019-07-08 10:22:23 -0700

    Updating dependencies.

--- webapp/src/client/client.js
+++ webapp/src/client/client.js
@@ -17,17 +17,13 @@ export default class Client {
     doPost = async (url, body, headers = {}) => {
         headers['X-Requested-With'] = 'XMLHttpRequest';
 
-        try {
-            const response = await request.
-                post(url).
-                send(body).
-                set(headers).
-                type('application/json').
-                accept('application/json');
+        const response = await request.
+            post(url).
+            send(body).
+            set(headers).
+            type('application/json').
+            accept('application/json');
 
-            return response.body;
-        } catch (err) {
-            throw err;
-        }
+        return response.body;
     }
 }
