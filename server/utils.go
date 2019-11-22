a671f09d54e08bd6a3a3249bfe854d3be6982f08
Vijay Annapureddy <vijayd@uber.com>
2019-11-22 07:44:39 -0800

    fix typo

--- server/utils.go
+++ server/utils.go
@@ -60,7 +60,7 @@ func decrypt(key []byte, text string) (string, error) {
 	}
 
 	if (len(decodedMsg) % aes.BlockSize) != 0 {
-		return "", errors.New("blocksize must be multipe of decoded message length")
+		return "", errors.New("blocksize must be multiple of decoded message length")
 	}
 
 	iv := decodedMsg[:aes.BlockSize]
