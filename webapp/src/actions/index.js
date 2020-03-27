115838ed4f5608eec07807dc07fbb14a302fc1f6
Daniel Espino García <larkox@gmail.com>
2020-03-27 16:26:56 +0100

    Add base route to plugin URL

--- webapp/src/actions/index.js
+++ webapp/src/actions/index.js
@@ -2,6 +2,7 @@
 // See License for license information.
 
 import {PostTypes} from 'mattermost-redux/action_types';
+import {getConfig} from 'mattermost-redux/selectors/entities/general';
 
 import Client from '../client';
 
@@ -9,7 +10,7 @@ export function startMeeting(channelId, force = false) {
     return async (dispatch, getState) => {
         try {
             const startFunction = force ? Client.forceStartMeeting : Client.startMeeting;
-            const meetingURL = await startFunction(channelId, true);
+            const meetingURL = await startFunction(channelId, getServerRoute(getState()), true);
             if (meetingURL) {
                 window.open(meetingURL);
             }
@@ -55,3 +56,18 @@ export function startMeeting(channelId, force = false) {
         return {data: true};
     };
 }
+
+export const getServerRoute = (state) => {
+    const config = getConfig(state);
+
+    let basePath = '';
+    if (config && config.SiteURL) {
+        basePath = new URL(config.SiteURL).pathname;
+
+        if (basePath && basePath[basePath.length - 1] === '/') {
+            basePath = basePath.substr(0, basePath.length - 1);
+        }
+    }
+
+    return basePath;
+};
--- webapp/src/client/client.js
+++ webapp/src/client/client.js
@@ -11,13 +11,13 @@ export default class Client {
         this.url = '/plugins/' + id;
     }
 
-    startMeeting = async (channelId, personal = true, topic = '', meetingId = 0, force = false) => {
-        const res = await doPost(`${this.url}/api/v1/meetings${force ? '?force=true' : ''}`, {channel_id: channelId, personal, topic, meeting_id: meetingId});
+    startMeeting = async (channelId, baseURL, personal = true, topic = '', meetingId = 0, force = false) => {
+        const res = await doPost(`${baseURL}${this.url}/api/v1/meetings${force ? '?force=true' : ''}`, {channel_id: channelId, personal, topic, meeting_id: meetingId});
         return res.meeting_url;
     }
 
-    forceStartMeeting = async (channelId, personal = true, topic = '', meetingId = 0) => {
-        const meetingUrl = await this.startMeeting(channelId, personal, topic, meetingId, true);
+    forceStartMeeting = async (channelId, baseURL, personal = true, topic = '', meetingId = 0) => {
+        const meetingUrl = await this.startMeeting(channelId, baseURL, personal, topic, meetingId, true);
         return meetingUrl;
     }
 }
