d810d59fb49e8392dc6661afbc38c408969ac78c
Daniel Espino García <larkox@gmail.com>
2020-04-22 13:49:56 +0200

    Change camera icon by zoom camera icon

--- webapp/src/components/icon.jsx
+++ webapp/src/components/icon.jsx
@@ -3,22 +3,19 @@
 
 import React from 'react';
 import {FormattedMessage} from 'react-intl';
-import {makeStyleFromTheme} from 'mattermost-redux/utils/theme_utils';
 
 import {Svgs} from '../constants';
 
 export default class Icon extends React.PureComponent {
     render() {
-        const style = getStyle();
-
         return (
             <FormattedMessage
                 id='zoom.camera.ariaLabel'
-                defaultMessage='camera icon'
+                defaultMessage='zoom camera icon'
             >
                 {(ariaLabel) => (
                     <span
-                        style={style.iconStyle}
+                        className='icon icon--standard'
                         aria-label={ariaLabel}
                         dangerouslySetInnerHTML={{__html: Svgs.VIDEO_CAMERA}}
                     />
@@ -27,12 +24,3 @@ export default class Icon extends React.PureComponent {
         );
     }
 }
-
-const getStyle = makeStyleFromTheme(() => {
-    return {
-        iconStyle: {
-            position: 'relative',
-            top: '-1px',
-        },
-    };
-});
