2b275b509ddca28fedc220b75c6a6e1a6be18603
Daniel Espino García <larkox@gmail.com>
2020-10-14 12:11:35 +0200

    Fix lint

--- server/telemetry.go
+++ server/telemetry.go
@@ -9,29 +9,29 @@ const (
 )
 
 func (p *Plugin) trackConnect(userID string) {
-	p.tracker.TrackUserEvent("connect", userID, map[string]interface{}{})
+	_ = p.tracker.TrackUserEvent("connect", userID, map[string]interface{}{})
 }
 
 func (p *Plugin) trackDisconnect(userID string) {
-	p.tracker.TrackUserEvent("disconnect", userID, map[string]interface{}{})
+	_ = p.tracker.TrackUserEvent("disconnect", userID, map[string]interface{}{})
 }
 
 func (p *Plugin) trackOAuthModeChange(method string) {
-	p.tracker.TrackEvent("oauth_mode_change", map[string]interface{}{
+	_ = p.tracker.TrackEvent("oauth_mode_change", map[string]interface{}{
 		"method": method,
 	})
 }
 
 func (p *Plugin) trackMeetingStart(userID, source string) {
-	p.tracker.TrackUserEvent("start_meeting", userID, map[string]interface{}{
+	_ = p.tracker.TrackUserEvent("start_meeting", userID, map[string]interface{}{
 		"source": source,
 	})
 }
 
 func (p *Plugin) trackMeetingDuplication(userID string) {
-	p.tracker.TrackUserEvent("meeting_duplicated", userID, map[string]interface{}{})
+	_ = p.tracker.TrackUserEvent("meeting_duplicated", userID, map[string]interface{}{})
 }
 
 func (p *Plugin) trackMeetingForced(userID string) {
-	p.tracker.TrackUserEvent("meeting_forced", userID, map[string]interface{}{})
+	_ = p.tracker.TrackUserEvent("meeting_forced", userID, map[string]interface{}{})
 }
