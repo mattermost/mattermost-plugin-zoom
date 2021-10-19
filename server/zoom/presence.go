c45d7fcf9908e8b29afc23013af102d772c911f8
Ossi Vaananen <ossi.vaananen@gmail.com>
2021-10-19 17:25:29 +0300

    Handle the meeting.participant_joined and meeting.participant_left events instead of user presence events

--- server/zoom/presence.go
+++ server/zoom/presence.go
@@ -1,21 +1,46 @@
 package zoom
 
-type PresenceEvent struct {
+type ParticipantJoinedLeftEvent struct {
 	EventType string	`json:"event"`
 	EventTimestamp int                  `json:"event_ts"`
-	Payload        PresenceEventPayload `json:"payload"`
+	Payload        ParticipantJoinedLeftEventPayload `json:"payload"`
 }
 
-type PresenceEventPayload struct {
-	AccountId string              `json:"account_id"`
-	Object    PresenceEventObject `json:"object"`
+type ParticipantJoinedLeftEventPayload struct {
+	AccountId string	`json:"account_id"`
+	Object ParticipantJoinedLeftEventObject 	`json:"object"`
 }
 
-type PresenceEventObject struct {
-	DateTime string	`json:"date_time"`
+type MeetingType int
+const (
+	Prescheduled MeetingType 	= 0
+	Instant						= 1
+	Scheduled					= 2
+	RecurringNoFixedTime		= 3
+	PersonalMeetingRoom			= 4
+	PersonalAudioConference		= 7
+	RecurringFixedTime			= 8
+)
+
+type ParticipantJoinedLeftEventObject struct {
+	Id string	`json:"id"`
+	Uuid string	`json:"uuid"`
+	HostId string	`json:"host_id"`
+	Topic string	`json:"topic""`
+	Type MeetingType	`json:"type"`
+	StartTime string	`json:"start_time""`
+	TimeZone string		`json:"timezone"`
+	Duration int		`json:"duration"`
+	Participant ParticipantJoinedLeftEventParticipant `json:"participant"`
+}
+
+type ParticipantJoinedLeftEventParticipant struct {
+	UserId string `json:"user_id"`
+	UserName string	`json:"user_name"`
+	Id string	`json:"id"`
+	LeaveTime string `json:"leave_time"`
+	LeaveReason string	`json:"leave_reason"`
 	Email string	`json:"email"`
-	Id string		`json:"id"`
-	PresenceStatus string	`json:"presence_status"`
+	RegistrantId string	`json:"registrant_id"`
+	ParticipantUserId string	`json:"participant_user_id"`
 }
-
-
