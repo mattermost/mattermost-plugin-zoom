03e623a8e0ede8e7177a9f1618e518480f531c9a
kshitij katiyar <90389917+Kshitij-Katiyar@users.noreply.github.com>
2023-02-28 13:18:44 +0530

    [MI-2825]:Fixed issue #240 edited the pretext (#5)

--- webapp/src/components/post_type_zoom/post_type_zoom.jsx
+++ webapp/src/components/post_type_zoom/post_type_zoom.jsx
@@ -79,7 +79,7 @@ export default class PostTypeZoom extends React.PureComponent {
         let content;
         let subtitle;
         if (props.meeting_status === 'STARTED') {
-            preText = 'I have started a meeting';
+            preText = post.message;
             if (this.props.fromBot) {
                 preText = `${this.props.creatorName} has started a meeting`;
             }
@@ -127,7 +127,7 @@ export default class PostTypeZoom extends React.PureComponent {
                 );
             }
         } else if (props.meeting_status === 'ENDED') {
-            preText = 'The meeting has ended';
+            preText = post.message;
             if (this.props.fromBot) {
                 preText = `${this.props.creatorName} has ended the meeting`;
             }
