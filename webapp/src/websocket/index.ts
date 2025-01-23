fe8397744f9c42ba441c560e0db39ba959787d57
Jesse Hallam <jesse.hallam@gmail.com>
2025-01-23 11:43:38 -0400

    Apply and enforce file-level license headers
    Use `mattermost-govet` and `eslint`'s `header/header` to ensure each
    file begins with the appropriate header, then fix all instances for
    built source code. (e2e code excepted)
    
    ```
    // Copyright (c) 2019-present Mattermost, Inc. All Rights Reserved.
    // See LICENSE.txt for license information.
    ```
    
    Note that this renames `LICENSE` to `LICENSE.txt` purely to align on the
    copyright messaging across repos.

--- webapp/src/websocket/index.ts
+++ webapp/src/websocket/index.ts
@@ -1,3 +1,6 @@
+// Copyright (c) 2019-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
 export function handleMeetingStarted(msg: {data: {meeting_url: string}}) {
     window.open(msg.data.meeting_url);
 }
