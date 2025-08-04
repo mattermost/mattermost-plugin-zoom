09202c6eb6bdb7dd66d7b8dac9508093c17a2452
raghavaggarwal2308 <raghav.aggarwal@brighscout.com>
2025-08-04 20:01:14 +0530

    [MM-62309] FIx issue with opening of multiple zoom meeting tabs

--- webapp/src/websocket/index.ts
+++ webapp/src/websocket/index.ts
@@ -1,6 +1,6 @@
 // Copyright (c) 2019-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-export function handleMeetingStarted(msg: {data: {meeting_url: string}}) {
-    window.open(msg.data.meeting_url);
+export function handleMeetingStarted(msg: { data: { meeting_url: string } }) {
+  window.open(msg.data.meeting_url, "_blank");
 }
